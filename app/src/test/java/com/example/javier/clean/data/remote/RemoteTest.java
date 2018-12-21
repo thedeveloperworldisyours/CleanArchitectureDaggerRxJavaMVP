2da17b39d991b2b8b3fc97dfc3fa8b93256e08f7
Javier <javiergonzalezcabezas@gmail.com>
2018-12-21 17:28:50 +0100

    added more test

--- app/src/test/java/com/example/javier/clean/data/remote/RemoteTest.java
+++ app/src/test/java/com/example/javier/clean/data/remote/RemoteTest.java
@@ -2,29 +2,32 @@ package com.example.javier.clean.data.remote;
 
 import com.example.javier.clean.data.entity.Topics;
 import com.google.gson.Gson;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import io.reactivex.observers.DisposableObserver;
-import io.reactivex.schedulers.Schedulers;
+import io.reactivex.Scheduler;
+import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.internal.schedulers.ExecutorScheduler;
+import io.reactivex.observers.TestObserver;
+import io.reactivex.plugins.RxJavaPlugins;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import static org.hamcrest.CoreMatchers.equalTo;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class RemoteTest {
 
-    List<Topics> mResultList;
+    List<Topics> resul;
     MockWebServer mMockWebServer;
 
     @Before
@@ -32,80 +35,74 @@ public class RemoteTest {
 
         Topics topics = new Topics(1, "Discern The Beach");
         Topics topicsTwo = new Topics(2, "Discern The Football Player");
-        mResultList = new ArrayList();
-        mResultList.add(topics);
-        mResultList.add(topicsTwo);
+        resul = new ArrayList();
+        resul.add(topics);
+        resul.add(topicsTwo);
 
         mMockWebServer = new MockWebServer();
-
     }
+
+    @BeforeClass
+    public static void setUpRxSchedulers() {
+        Scheduler immediate = new Scheduler() {
+            @Override
+            public Disposable scheduleDirect(@NonNull Runnable run, long delay, @NonNull TimeUnit unit) {
+                // this prevents StackOverflowErrors when scheduling with a delay
+                return super.scheduleDirect(run, 0, unit);
+            }
+
+            @Override
+            public Worker createWorker() {
+                return new ExecutorScheduler.ExecutorWorker(Runnable::run);
+            }
+        };
+
+        RxJavaPlugins.setInitIoSchedulerHandler(scheduler -> immediate);
+        RxJavaPlugins.setInitComputationSchedulerHandler(scheduler -> immediate);
+        RxJavaPlugins.setInitNewThreadSchedulerHandler(scheduler -> immediate);
+        RxJavaPlugins.setInitSingleSchedulerHandler(scheduler -> immediate);
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler(scheduler -> immediate);
+    }
+
     @Test
     public void severCallWithSuccessful() {
         //Given
         String url = "https://guessthebeach.herokuapp.com/api/";
-        mMockWebServer.enqueue(new MockResponse().setBody(new Gson().toJson(mResultList)));
+        mMockWebServer.enqueue(new MockResponse().setBody(new Gson().toJson(resul)));
         Retrofit retrofit = new Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(mMockWebServer.url(url))
                 .build();
         RemoteApi remoteDataSource = new RemoteImpl(retrofit);
+        TestObserver<List<Topics>> topicsObservable = remoteDataSource.getTopicsRx().test();
 
-        //When
-        remoteDataSource.getTopicsRx().subscribeOn(Schedulers.io())
-                .subscribe(new DisposableObserver<List<Topics>>() {
-                    @Override
-                    public void onNext(List<Topics> topics) {
-                        assertThat("Topics count is an unexpected value", topics.size(), is(equalTo(5)));
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                        Assert.fail("Unxpected response :" + e.getMessage());
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
+        remoteDataSource.getTopicsRx().subscribe(topicsObservable);
 
 
-                });
+        topicsObservable.hasSubscription();
+        assertThat(topicsObservable.values().get(0).size(), is(2));
+        assertThat(topicsObservable.values().get(0).get(0).getId(), is(1));
+        assertThat(topicsObservable.values().get(0).get(1).getId(), is(2));
+
+        assertThat(topicsObservable.values().get(0).get(0).getName(), is("Discern The Beach"));
+        assertThat(topicsObservable.values().get(0).get(1).getName(), is("Discern The Football Player"));
     }
 
     @Test
     public void serverCallWithError() {
         //Given
         String url = "dfdf/";
-        mMockWebServer.enqueue(new MockResponse().setBody(new Gson().toJson(mResultList)));
+        resul = new ArrayList();
+        mMockWebServer.enqueue(new MockResponse().setBody(new Gson().toJson(resul)));
         Retrofit retrofit = new Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(mMockWebServer.url(url))
                 .build();
         RemoteApi remoteDataSource = new RemoteImpl(retrofit);
-
-        //When
-        remoteDataSource.getTopicsRx().subscribeOn(Schedulers.io())
-                .subscribe(new DisposableObserver<List<Topics>>() {
-                    @Override
-                    public void onNext(List<Topics> topics) {
-                        assertThat("Topics count is an unexpected value", topics.size(), is(equalTo(2)));
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                        Assert.fail("Unxpected response :" + e.getMessage());
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-
-
-                });
+        TestObserver<List<Topics>> topicsObservable = remoteDataSource.getTopicsRx().test();
+        topicsObservable.hasSubscription();
+        assertThat(topicsObservable.values().get(0).size(), is(0));
     }
 }
